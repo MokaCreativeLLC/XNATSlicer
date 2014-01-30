4cb7d7386720b79311c12139aef408b3418f9a70
Sunil Kumar <sunil.kumar@alumni.duke.edu>
2014-01-29 16:21:53 -0800

    Refactoring settings.

---
+++ tester.py
@@ -0,0 +1,10 @@
+class tester(object):
+    """
+    """
+    TEST = ['TEST']
+    def __init__(self):
+        print "iniot"
+        
+    def test(self):
+        self.TEST.append('test2')
+        print self.TEST
